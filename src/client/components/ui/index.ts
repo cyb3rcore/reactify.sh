46f68b368c0f2127fc74302c500256acab32d557
teneburu <teneburu@proton.me>
2026-07-12 00:45:31 +0200

    chore: set up Panda CSS, Park UI, and project dependencies

---
+++ src/client/components/ui/index.ts
@@ -0,0 +1,13 @@
+export { AbsoluteCenter, type AbsoluteCenterProps } from "./absolute-center";
+export { Badge, type BadgeProps } from "./badge";
+export { Button, ButtonGroup, type ButtonGroupProps, type ButtonProps } from "./button";
+export * as Card from "./card";
+export { Group, type GroupProps } from "./group";
+export { Heading, type HeadingProps } from "./heading";
+export { Icon, type IconProps } from "./icon";
+export { IconButton, type IconButtonProps } from "./icon-button";
+export { Loader, type LoaderProps } from "./loader";
+export { Skeleton, SkeletonCircle, SkeletonText, type SkeletonCircleProps, type SkeletonProps, type SkeletonTextProps } from "./skeleton";
+export { Span, type SpanProps } from "./span";
+export { Spinner, type SpinnerProps } from "./spinner";
+export { Text, type TextProps } from "./text";
---
+++ src/client/theme/recipes/index.ts
@@ -0,0 +1,23 @@
+import { spinner } from "./spinner";
+import { absoluteCenter } from "./absolute-center";
+import { group } from "./group";
+import { skeleton } from "./skeleton";
+import { icon } from "./icon";
+import { badge } from "./badge";
+import { card } from "./card";
+import { text } from "./text";
+import { heading } from "./heading";
+import { button } from "./button";
+export const recipes = {
+  button,
+  heading,
+  text,
+  card,
+  badge,
+  icon,
+  skeleton,
+  group,
+  absoluteCenter,
+  spinner
+}
+export const slotRecipes = {}
